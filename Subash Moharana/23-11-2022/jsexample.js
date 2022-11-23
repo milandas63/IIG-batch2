8afec0232a9d63ba10001db0444ee00ad8f6867d
moharanasubash123 <115611240+moharanasubash123@users.noreply.github.com>
2022-11-23 04:54:49 -0800

    Add files via upload

---
+++ Subash Moharana/23-11-2022/jsexample.js
@@ -0,0 +1,10 @@
+var today= new Date();
+var dd = today.getDate();
+var mm=today.getMonth();
+var yyyy= today.getFullYear();
+var HH = today.getHours();
+var MM = today.getMinutes();
+var ss= today.getSeconds();
+console.log("today date is " + dd + "/" + mm + "/" + yyyy);
+console.log("time is :- " + HH + ":" + MM + ":" + ss);
+
